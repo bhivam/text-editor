1827d17b21d449e3c7911a8ed000593f096bdf24
bhivam <shivamkajaria@gmail.com>
2024-11-07 20:48:14 -0500

    switched to use raw tcp

--- server/main.go
+++ server/main.go
@@ -4,15 +4,40 @@ import (
 	"fmt"
 	"io"
 	"log"
-
-	"github.com/gliderlabs/ssh"
+	"net"
 )
 
-func main() {
-	ssh.Handle(func(s ssh.Session) {
-		io.WriteString(s, fmt.Sprintf("Hello %s\n", s.User()))
-	})
+func handleConnection(conn net.Conn) {
+	fmt.Println("Handling new connection from", conn.RemoteAddr())
+	defer conn.Close()
+	buf := [512]byte{}
+	for {
+		fmt.Fprintln(conn, "Hello from the server!")
+		_, err := conn.Read(buf[:])
+		if err == io.EOF {
+			log.Printf("Connection closed by remote host: %v", conn.RemoteAddr())
+			return
+		}
 
-	log.Println("starting ssh server on port 2222...")
-	log.Fatal(ssh.ListenAndServe("localhost:2222", nil))
+		fmt.Printf("Received %v bytes from %v: %v\n", len(buf), conn.RemoteAddr(), string(buf[:]))
+
+		if err != nil {
+			log.Printf("Error reading from connection: %v", err)
+			return
+		}
+	}
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		// handle error
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+		go handleConnection(conn)
+	}
 }
